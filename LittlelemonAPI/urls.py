ef4301e6ed8cb66498c4492de35c05c503a80f63
Kiruba <kirubakaran260202@gmail.com>
2023-12-22 19:21:05 +0530

    finalcommit1

--- LittlelemonAPI/urls.py
+++ LittlelemonAPI/urls.py
@@ -2,8 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    #path('categories', views.CategoriesView.as_view()),
-    #path('menu-items', views.MenuItemsView.as_view()),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('menu-items', views.MenuItemsView.as_view()),
